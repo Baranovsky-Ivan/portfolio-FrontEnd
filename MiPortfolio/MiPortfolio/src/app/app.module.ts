51d2928a7c38912ad922ccfcd42c0939ac801ace
Baranovsky.Ivan <b94.ivan@gmail.com>
2022-05-17 17:51:04 -0300

    angular

--- MiPortfolio/MiPortfolio/src/app/app.module.ts
+++ MiPortfolio/MiPortfolio/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BarraComponent } from './componentes/barra/barra.component';
+import { BannerComponent } from './componentes/banner/banner.component';
+import { AcercaDeComponent } from './componentes/acerca-de/acerca-de.component';
+import { AgregarQuitarComponent } from './componentes/plantillas/agregar-quitar/agregar-quitar.component';
+import { EstudiosComponent } from './componentes/plantillas/estudios/estudios.component';
+import { HardYSoftSkillsComponent } from './componentes/plantillas/hard-y-soft-skills/hard-y-soft-skills.component';
+import { ProyectosComponent } from './componentes/plantillas/proyectos/proyectos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BarraComponent,
+    BannerComponent,
+    AcercaDeComponent,
+    AgregarQuitarComponent,
+    EstudiosComponent,
+    HardYSoftSkillsComponent,
+    ProyectosComponent,
   ],
   imports: [
     BrowserModule,
